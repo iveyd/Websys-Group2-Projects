f1c329a0dacbc3bfaa1902877782ffc5d52f7990
Joseph Noel <jtnoel96@gmail.com>
2016-11-30 17:43:20 -0500

    Registration form php

---
+++ resources/php/register.php
@@ -0,0 +1,36 @@
+<?php
+	session_start();
+
+  // Connect to the database
+  try {
+    $dbname = 'RPIRPG';
+    $user = 'root';
+    $pass = 'root';
+    $dbconn = new PDO('mysql:host=localhost;dbname='.$dbname, $user, $pass);
+  }
+  catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+  }
+
+  if ($dbconn && isset($_POST['create']) && $_POST['create'] == 'Create') {
+  	if (!isset($_POST['username']) || !isset($_POST['password']) || 
+  			!isset($_POST['confirm'])  || !isset($_POST['email'])) {
+  		$msg = "Please fill in all fields.";
+  	} else if ($_POST['password'] !== $_POST['confirm']) {
+  		$msg = "Passwords must match.";
+  	} else {
+  		// Generate salt
+  		$salt = hash('sha256', uniqid(mt_rand(), true));
+  		// Apply salt before hash
+  		$salted = hash('sha256', $salt . $_POST['password']);
+  		// Store salt
+  		$stmt = $dbconn->prepare("INSERT INTO `users` (`email`, `username`, `password`, `salt`) VALUES (:email, :username, :password, :salt");
+  		$stmt->execute(array(':email' => $_POST['email'],
+  												 ':username' => $_POST['username'],
+  												 ':password' => $salted,
+  												 ':salt' => $salt));
+  		header('Location: ../../char_create.html');
+
+  	}
+  }
+?>
