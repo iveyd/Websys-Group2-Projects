575961e292cc83b2a9240103313a4a5725b43fe5
Connor <connor.stalker95@gmail.com>
2016-12-02 12:36:35 -0500

    got settings reset button to actually reset character

--- resources/php/settings.php
+++ resources/php/settings.php
@@ -7,31 +7,35 @@
   $db = "RPIRPG";
 
   try {
-  	$dbh = new PDO("mysql:host=$host", $username, $password);
+  	$dbh = new PDO("mysql:host=$host;dbname=".$db, $username, $password);
+  	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } 
   catch(PDOException $e) {
   	echo 'ERROR: ' . $e->getMessage();
   }
 
+try {
   if(isset($_POST['charreset']))
   {
 
-  	$sql = "UPDATE `character`
-			SET `charname` = '', 
-				`img` = '',
-				`major` = '',
-				`semester` = '',
-				`courses` = '',
-				`year` = '',
-				`passed` = 0,
-				`failed` = 0
-			WHERE `character`.`charid` = 0;";
-	$update = $dbh->prepare($sql);
-  	$update->execute();
+  	$sql = "UPDATE `character` 
+  			SET `charname` = '', 
+  				`img` = '', 
+  				`major` = '', 
+  				`semester` = '', 
+  				`courses` = '', 
+  				`year` = '', 
+  				`passed` = '0', 
+  				`failed` = '0';";
+	$dbh->query($sql);
+  	//$update->execute();
 
-  	print("Return number of rows that were updated:\n");
-	$count = $update->rowCount();
-	print("Updated $count rows.\n");	
+	// $count = $update->rowCount();
+	print("Updated rows.\n");	
   }
+}
+catch(PDOException $e) {
+	echo 'ERROR: ' . $e->getMessage();
+}
   
 ?>
