7b30ac5fad8834ec6ce9be10a79bc807f4e54000
David <iveyd@rpi.edu>
2016-12-04 16:14:02 -0500

    adding php functionality to char_create

--- char_create.php
+++ char_create.php
@@ -1,24 +1,24 @@
-<?php
-  session_start();
+<?php 
 
-  if (isset($_POST['submit']) && $_POST['submit'] == 'Create!') {
+  try {
+    $dbname = 'RPIRPG';
+    $user = 'root';
+    $pass = 'test_password';
+    $dbconn = new PDO('mysql:host=localhost;dbname='.$dbname, $user, $pass);
+  }
+  catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+  }
 
+  if (isset($_POST['submit']) && ($_POST['submit'] == 'Create!')) {
+    $name = $_POST['name'];
+    $avatar = $_POST['avatar'];
+    echo "fuck bitches. get money.";
+    $stmt = $dbconn->prepare("INSERT INTO character (charname, img) VALUES (:charname, :img)");
+    $stmt->execute(array(':charname' => $name, ':img' => $avatar));
 
-
-
-  if (isset($_POST['avatar']) || empty($_POST['avatar'])) {
-      echo "Please Name Your Character.";
-    }
-
-  if (isset($_POST['avatar']) || empty($_POST['name'])) {
-      echo "Please Choose an Avatar.";
-    }
-
-
-
-
-}
-    
+    header("Location: main.php");
+  }
 
 ?>
 <!doctype html>
@@ -63,55 +63,55 @@
     <img id="title" src="resources/images/Create-Your-Character.png" alt="Create Your Character">
 
 
-    <form id="charCreateForm" action="char_create.php">
+    <form id="charCreateForm" action="char_create.php" method="POST">
       <div class="form-inline">
       <div class="text-center">
         <img src="resources/images/Name-Your-Character.png" alt="Name Your Character">
       </div>
       <div class="text-center">
-        <input id="nameField" class="form-control" type="text" name="name" autofocus>
+        <input id="nameField" class="form-control" type="text" name="name" autofocus required>
       </div>
       <div class="text-center">
         <img src="resources/images/Choose-Your-Avatar.png" alt="Choose Your Avatar">
       </div>
         <div id="avatarSelect">
           <label>
-            <input type="radio" name="avatar" value="light_male" />
+            <input type="radio" name="avatar" value="light_male.png"/>
             <img class="avatar" src="resources/images/light_male.png" alt="light_male">
           </label>
           <label>
-            <input type="radio" name="avatar" value="light_female" />
+            <input type="radio" name="avatar" value="light_female.png" />
             <img class="avatar" src="resources/images/light_female.png" alt="light_female">
           </label>
 
           <label>
-            <input type="radio" name="avatar" value="dark_male" />
+            <input type="radio" name="avatar" value="dark_male.png" />
             <img class="avatar" src="resources/images/dark_male.png" alt="dark_male">
           </label>
           <label>
-            <input type="radio" name="avatar" value="dark_female" />
+            <input type="radio" name="avatar" value="dark_female.png" />
             <img class="avatar" src="resources/images/dark_female.png" alt="dark_female">
           </label>
 
           <label>
-            <input type="radio" name="avatar" value="tan_male" />
+            <input type="radio" name="avatar" value="tan_male.png" />
             <img class="avatar" src="resources/images/tan_male.png" alt="tan_male">
           </label>
           <label>
-            <input type="radio" name="avatar" value="tan_female" />
+            <input type="radio" name="avatar" value="tan_female.png" />
             <img class="avatar" src="resources/images/tan_female.png" alt="tan_female">
           </label>
 
           <label>
-            <input type="radio" name="avatar" value="pale_male" />
+            <input type="radio" name="avatar" value="pale_male.png" />
             <img class="avatar" src="resources/images/pale_male.png" alt="pale_male">
           </label>
           <label>
-            <input type="radio" name="avatar" value="pale_female" />
+            <input type="radio" name="avatar" value="pale_female.png" />
             <img class="avatar" src="resources/images/pale_female.png" alt="pale_female">
           </label>
         </div>
-        <input id="submit" type="submit" class="btn btn-success" value="Create!"/>
+        <input id="submit" type="submit" class="btn btn-success" name="submit" value="Create!" onclick="validate();"/>
       </div>
     </form>
   </div>
@@ -123,6 +123,14 @@
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script>
+    function validate(formObj) {
+      if (document.forms["charCreateForm"]["avatar"].value == "") {
+        $("#avatarSelect").css("box-shadow", "0px 0px 2px 2px #222222")
+      }
+     }
+
+  </script>
 </body>
 </html>
 
